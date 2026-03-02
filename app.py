f7bb833c3e6089b3a8bf56a067a7271f25400da4
nandhini017 <nandhinisathish017@gmail.com>
2026-03-02 12:10:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -151,7 +151,5 @@ def logout():
     return redirect(url_for('login'))
 
 # ---------------- RUN APP ----------------
-import os
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=True)
